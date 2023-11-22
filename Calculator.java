f185224b5cf64918428bb816dbc40c66f69edbc0
maverick-farhan <mdfarhanofficial@gmail.com>
2023-11-22 10:03:00 +0530

    Calculator in java added

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean flag = true;
+        while(flag){
+            System.out.println("Enter Operator(+,-,/,*) number1 number2");
+            //ch is operator for the arithmetic to perform
+            String ch = in.next();
+            double num1 = in.nextFloat();
+            double num2 = in.nextFloat();
+            switch(ch){
+                case "+":
+                    System.out.println("Answer: "+ (num1+num2)+"\n");
+                    break;
+                case "-":
+                    System.out.println("Answer: "+ (num1-num2)+"\n");
+                    break;
+                case "*":
+                    System.out.println("Answer: "+ (num1*num2)+"\n");
+                    break;
+
+                case "/":
+                    System.out.println("Answer: "+ (num1/num2)+"\n");
+                    break;
+                default:
+                    System.out.println("Enter valid operations");
+            }
+        }
+    }
+}
