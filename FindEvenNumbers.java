58c229a31aa149dae6ec4906da7e3d1608630b4d
maverick-farhan <mdfarhanofficial@gmail.com>
2023-11-23 14:52:01 +0530

    Added Find Even Digit integers in the array

---
+++ FindEvenNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class FindEvenNumbers {
+// Leetcode: 1295. Find Numbers with Even Number of Digits
+public static void main(String[] args) {
+    int[] arr = {1,200,20,100,12,345,2,6,7896};
+    System.out.println("Even digit numbers are : " + countEvenDigits(arr));
+}
+    static int countEvenDigits(int[] arr){
+    int count = 0;
+    int evenDigits = 0;
+    for(int i = 0;i<arr.length;i++)
+    {
+        while(arr[i]>0) {
+            arr[i]/=10;
+            count+=1;
+        }
+        if (count % 2 == 0) {
+            evenDigits += 1;
+        }
+        count=0;
+    }
+        return evenDigits;
+}
+
+}
