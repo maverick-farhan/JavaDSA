5b90ea2b753abbcc70c7d0c8f97959f344d20239
maverick-farhan <mdfarhanofficial@gmail.com>
2023-11-23 11:53:18 +0530

    ReverseArray problem added

---
+++ ReverseArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = {12,2,4,14,5,10};
+        System.out.println(Arrays.toString(arr));
+        int start = 0;
+        int end = arr.length - 1;
+        while(start<=end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr,int start,int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
