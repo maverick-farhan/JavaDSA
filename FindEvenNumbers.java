e1e5a44500ecc02dd1d6c2bbcd11adf01b333af3
maverick-farhan <mdfarhanofficial@gmail.com>
2023-11-23 17:41:37 +0530

    Leetcode problem 1295 added

--- FindEvenNumbers.java
+++ FindEvenNumbers.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
-public class FindEvenNumbers {
 // Leetcode: 1295. Find Numbers with Even Number of Digits
+public class FindEvenNumbers {
 public static void main(String[] args) {
     int[] arr = {1,200,20,100,12,345,2,6,7896};
     System.out.println("Even digit numbers are : " + countEvenDigits(arr));
